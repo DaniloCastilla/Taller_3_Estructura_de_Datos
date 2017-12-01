3d77763c6b13dbd82ba68da1bd5decf11c5ba41d
Eddycas <32119229+Eddycas@users.noreply.github.com>
2017-12-01 01:13:08 -0500

    Add files via upload

---
+++ Taller_3 - Estructura de Datos (Danilo Castilla)/Punto_3 - C++/Punto_3.cpp
@@ -0,0 +1,85 @@
+/*
+ * Punto3.cpp
+ *
+ *  Created on: 26 nov. 2017
+ *      Author: Danicas99
+ */
+
+#include "Punto3.h"
+
+#include<iostream>
+#include <string>
+#include <stdlib.h>
+#include <sstream>
+#include <conio.h>
+
+using namespace std;
+
+string NumberToString(int pNumber) //Convert numbers to string to be able to print
+{
+ ostringstream oOStrStream;
+ oOStrStream << pNumber;
+ return oOStrStream.str();
+}//end convertion
+
+int main(int argc, char* argv[]){ //Main method
+int m = 0 ;
+int npar = 0 ;
+int nimpar = 0;
+int npos = 0;
+int nneg = 0 ;
+
+	string cpar, cimpar, cpos, cneg, x;
+	cout << "ingrese las dimenciones de la Matriz: \n";
+	cin >> m ;
+	int matriz [m][m];
+	cout <<"los numeros aleatorios son: \n\n";
+
+	for (int i= 0 ;i<m ;i++){
+		for (int j= 0 ;j<m ;j++){
+			matriz[i][j] = rand()% 50 - 10;
+		}
+	}
+
+	for (int i=0;i<m ;i++){
+		x = "";
+		for (int j= 0 ;j<m ;j++){
+			x = x + "[" + NumberToString(matriz[i][j])+ "]";
+		}
+		cout << x + "\n";
+	}
+
+	for(int i = 0;i<m;i++){ //Cycle for to find the requirements
+		for(int j = 0; j<m;j++){
+			if(matriz[i][j]%2 == 0){
+				npar++;
+				cpar = cpar + NumberToString(matriz[i][j]) + ",";
+			}
+			else if (matriz[i][j]%2 != 0){
+				nimpar ++ ;
+				cimpar = cimpar+ NumberToString(matriz[i][j]) + ",";
+			}
+			if(matriz[i][j]< 0 ){
+				nneg ++;
+				cneg = cneg + NumberToString(matriz[i][j]) + ",";
+			}
+			else if(matriz[i][j]>= 0){
+				npos++;
+				cpos = cpos + NumberToString(matriz[i][j]) + ",";
+			}
+		}
+	}
+	cout <<"\n\nla cantidad de numeros pares encontrados son: " + NumberToString(npar )+ "\n\n";
+	cout <<"la cantidad de numeros impares encontrados son: " + NumberToString(nimpar) + "\n\n";
+	cout <<"la cantidad de numeros negativos encontrados son: " + NumberToString(nneg) + "\n\n";
+	cout <<"la cantidad de numeros positivos encontrados son: " + NumberToString(npos) + "\n\n";
+	cout <<"los numeros pares encontrados son: "+ cpar + "\n\n";
+	cout <<"los numeros impares encontrados son:" + cimpar + "\n\n";
+	cout <<"los numeros negativos encontrados son:" + cneg + "\n\n";
+	cout <<"los numeros positivos encontrados son:" + cpos + "\n\n";
+
+	getch();
+	return 0;
+
+}//end Main
+
---
+++ Taller_3 - Estructura de Datos (Danilo Castilla)/Punto_4 - C++/src/Punto_4.cpp
@@ -0,0 +1,89 @@
+//============================================================================
+// Name        : Punto_4.cpp
+// Author      : Danicas
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <conio.h>
+
+ using namespace std;
+
+ int main(){//Main method
+
+	 	int x;
+	  	string Cadena;
+	    vector<int> vect;
+	    cout <<"ingrese el tamaño de la matriz: " << endl;
+	    cin >> x;
+	    int Vector[x*x];
+	    int Vector_2[100][100];
+	    int tamain = (sizeof Vector/sizeof * Vector);
+	    cout << "Digite la cadena de caracteres separado por comas: \n";
+	    cin >> Cadena;
+
+	    stringstream Separated(Cadena);
+
+	    int i;
+
+	    while (Separated >> i){//Separated with Comas
+	         	 vect.push_back(i);
+	         if (Separated.peek() == ',')
+	        	 Separated.ignore();
+	    }//end While
+
+	    int Tm = vect.size();
+
+	    for (i=0; i< Tm; i++){
+
+	     	Vector[i] = vect.at(i);
+
+	    }
+
+	    cout<<endl;
+	    int c = 0;
+	    do{
+	    for (int i = 0; i < x; i++){
+
+			for (int j = 0; j < x ; j++) {
+
+				Vector_2[i][j] = Vector[c];
+				c++;
+			}
+
+		}
+
+	  }while(c<tamain);
+
+	    for (int k = 0; k < x; k++) {
+	    	for (int j = 0; j < x; j++) {
+	    		cout << "["<<Vector_2[k][j]<<"]";
+
+	    			}
+
+	    	cout<<endl;
+
+	    	}
+
+	    cout << endl;
+	    cout << "Matriz Transpuesta"<<endl;
+
+	    for (int k = 0; k < x; k++) {
+	    	 for (int j = 0; j < x; j++) {
+	    	    cout << "["<<Vector_2[j][k]<<"]";
+
+	    	   }
+
+	    	   cout<<endl;
+
+	    	}
+
+	    getch();
+	    return 0;
+
+ }
